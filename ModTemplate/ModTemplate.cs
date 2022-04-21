edf917d5e84227c3e43acd7db51b61f4cc87fbe8
LikeMauro <104032732+LikeMauro@users.noreply.github.com>
2022-04-21 02:18:27 +0200

    idk what 2 say

--- ModTemplate/ModTemplate.cs
+++ ModTemplate/ModTemplate.cs
@@ -1,5 +1,6 @@
 ﻿using OWML.Common;
 using OWML.ModHelper;
+using UnityEngine.InputSystem;
 
 namespace ModTemplate
 {
@@ -12,19 +13,12 @@ namespace ModTemplate
             // Use Start() instead.
         }
 
-        private void Start()
+        private void Update()
         {
-            // Starting here, you'll have access to OWML's mod helper.
-            ModHelper.Console.WriteLine($"My mod {nameof(ModTemplate)} is loaded!", MessageType.Success);
-
-            // Example of accessing game code.
-            LoadManager.OnCompleteSceneLoad += (scene, loadScene) =>
+            if (Keyboard.current.uKey.wasPressedThisFrame)
             {
-                if (loadScene != OWScene.SolarSystem) return;
-                var playerBody = FindObjectOfType<PlayerBody>();
-                ModHelper.Console.WriteLine($"Found player body, and it's called {playerBody.name}!",
-                    MessageType.Success);
-            };
+                FindObjectOfType<DeathManager>()?.KillPlayer(deathType: DeathType.Meditation);
+            }
         }
     }
 }
